b306d29119a67062008cf3f4ac5405b576975499
Justin Dickinson <justin@vimeo.com>
2013-04-13 19:42:11 -0400

    Say hi to YDNTLYR

---
+++ README.md
@@ -0,0 +1,6 @@
+YOU DONT NEED TO LEAVE YOUR ROOM
+
+An image tiler by @jmdickinson. Hit the base URL for a random image. Pass your favorite image by url variable. See it tile. Become mesmerized by gif motion.
+
+If you do something with this, let me know.
+
---
+++ com/index.php
@@ -0,0 +1,91 @@
+<?php
+    require_once "imgs.php";
+
+    if (isset($_GET['b'])) {
+        $bg = 'http://' . $_GET['b'];
+        $alt = '#';
+        $url = 'http://youdontneedtoleaveyourroom.com/?b='. $_GET['b'];
+    } elseif (isset($_GET['i'])) {
+        $bg = 'http://stuff.justindickinson.com/random/' . $imgs[$_GET['i']];
+        $alt = $_GET['i'];
+        $url = 'http://youdontneedtoleaveyourroom.com/?i='. $_GET['i'];
+    } else {
+        $r = rand(0,count($imgs) - 1);
+        $bg = 'http://stuff.justindickinson.com/random/' . $imgs[$r];
+        $alt = $r;
+        $url = 'http://youdontneedtoleaveyourroom.com/?i='. $r;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="title" content="YOU•DONT•NEED•TO•LEAVE•YOUR•ROOM" />
+    <meta name="description" content="You don’t need to leave your room.
+                                            Remain sitting at your table and listen.
+                                        Don’t even listen, simply wait.
+                                    Don’t even wait.
+                                                Be quite still and solitary.
+                                The world will freely offer itself to you.
+                                            To be unmasked, it has no choice.
+                                                It will roll in ecstasy at your feet." />
+
+    <title>YOU&bull;DONT&bull;NEED&bull;TO&bull;LEAVE&bull;YOUR&bull;ROOM</title>
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <style type="text/css">
+        body {
+            background: transparent url('<?= $bg ?>') repeat 0 0;
+        }
+
+        h1 {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+        }
+
+        footer {
+            position: absolute;
+            bottom: 20px;
+            right: 20px;
+        }
+
+        h1 a,
+        footer a {
+            display: block;
+            padding: 10px;
+            background: #000;
+            opacity: 0.1;
+            font-family: sans-serif;
+            text-decoration: none;
+            -moz-transition: opacity 0.1s linear;
+            -ms-transition: opacity 0.1s linear;
+            -o-transition: opacity 0.1s linear;
+            -webkit-transition: opacity 0.1s linear;
+            transition: opacity 0.1s linear;
+            color: #fff;
+        }
+
+        h1 a {
+            font-size: 48px;
+        }
+
+        footer a {
+            font-size: 16px;
+        }
+
+        h1 a:hover,
+        footer a:hover {
+            opacity: 0.3;
+        }
+    </style>
+</head>
+
+<body>
+    <h1><a href="<?= $url ?>"><?= $alt ?></a></h1>
+    <footer><a href="http://twitter.com/jmdickinson" target="_blank">@jmdickinson</a></footer>
+</body>
+</html>
